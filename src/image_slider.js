0e8856df1f0d3be8f1a90ccd0da6a132d9e332d3
Jonathan Lui <jonslui@gmail.com>
2021-06-08 14:25:50 -0600

    image carousel is fully functional

--- src/image_slider.js
+++ src/image_slider.js
@@ -1,4 +1,4 @@
-function populateSlider(image, container) {
+function populateSlider(image, container, index) {
   const imageContainer = document.createElement('div');
   imageContainer.setAttribute('class', 'imageContainer');
 
@@ -7,15 +7,53 @@ function populateSlider(image, container) {
   const element = document.createElement('img');
   element.setAttribute('class', 'image');
   element.setAttribute('alt', imageALT);
+  element.id = index;
   element.src = imageSRC;
+
+  if (index !== 0) {
+    element.style.display = 'none';
+  }
 
   imageContainer.appendChild(element);
   container.appendChild(imageContainer);
 }
 
+function nextImage(HTMLcollection) {
+  let i = 0;
+
+  while (HTMLcollection[i].style.display !== '') {
+    i += 1;
+  }
+
+  HTMLcollection[i].style.display = 'none';
+
+  if (i === HTMLcollection.length - 1) {
+    HTMLcollection[0].style.display = '';
+  } else {
+    HTMLcollection[i + 1].style.display = '';
+  }
+}
+
+function lastImage(HTMLcollection) {
+  let i = 0;
+
+  while (HTMLcollection[i].style.display !== '') {
+    i += 1;
+  }
+
+  HTMLcollection[i].style.display = 'none';
+
+  if (i === 0) {
+    HTMLcollection[HTMLcollection.length - 1].style.display = '';
+  } else {
+    HTMLcollection[i - 1].style.display = '';
+  }
+}
+
 function createImageSlider(array, container) {
   const sliderContainer = document.createElement('div');
   sliderContainer.setAttribute('class', 'sliderContainer');
+
   // buttons to move images
   const leftButton = document.createElement('a');
   leftButton.setAttribute('class', 'nextPhotoButton');
@@ -23,8 +61,17 @@ function createImageSlider(array, container) {
   rightButton.setAttribute('class', 'nextPhotoButton');
 
   sliderContainer.appendChild(leftButton);
-  array.forEach((image) => populateSlider(image, sliderContainer));
+  array.forEach((image, index) => populateSlider(image, sliderContainer, index));
   sliderContainer.appendChild(rightButton);
+
+  // On button click: pass array of pointers to images to function and currently visible image
+  const imageHTMLCollection = sliderContainer.getElementsByClassName('image');
+  leftButton.addEventListener('click', () => {
+    lastImage(imageHTMLCollection);
+  });
+  rightButton.addEventListener('click', () => {
+    nextImage(imageHTMLCollection);
+  });
 
   container.appendChild(sliderContainer);
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import createImageSlider from './image_slider';
 
 const contentContainer = document.getElementById('content');
 const imageArray = [['images/cat.jpg', 'Cat'],
-  ['images/cat2.jpg', 'Cat 2']];
+  ['images/cat2.jpg', 'Cat 2'], ['images/cat3.jpg', 'Cat 3']];
 
 createImageSlider(imageArray, contentContainer);
